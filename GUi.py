6d358dfbe1b67409b12601ccffc86cd74f05b2ba
Mark Musil <musilmark25@gmail.com>
2024-03-06 10:30:39 -0500

    non working state

--- GUi.py
+++ GUi.py
@@ -1,57 +1,55 @@
-
+from functional import *
+QT = True
+if QT:
+    import PySimpleGUIQt as sg
+else:
+    import PySimpleGUI as sg
 
 def entry_point():
 
     # Create some elements
     layout = [
             [sg.Multiline(size=(110, 30), font='courier 10', background_color='black', text_color='white', key='-MLINE-')],
-            [sg.Text('Select an input device:')],
+            [sg.Text('Select an audio input device:')],
             [sg.Listbox(
-                     values=text_list,
+                     values=list_input_devices(show=False),
                      size=(400, 20 * len(text_list)) if QT else (15, len(text_list)),
                      change_submits=True,
                      bind_return_key=True,
                      auto_size_text=True,
-                     default_values=text_list[2],
-                     key='_FLOATING_LISTBOX_',
+                     default_values=input_devices[0],
+                     key='-INPUT_DEVICE-',
                      enable_events=True)],
-            [sg.Text('Select an output device:')],
+            [sg.Text('Select an audio output device:')],
             [sg.Listbox(
-                    values=moods,
+                    values=list_output_devices(show=False),
                     size=(400, 20 * len(moods)) if QT else (15, len(moods)),
                     change_submits=True,
                     bind_return_key=True,
                     auto_size_text=True,
-                    default_values="normal, reserved, friendly",
-                    key='-MOOD-', 
+                    default_values=output_devices[0],
+                    key='-OUTPUT_DEVICE-', 
                     enable_events=True)],
             [sg.Button('Start', bind_return_key=True), sg.Button('Close')],
+            [sg.Button('Stop', bind_return_key=True), sg.Button('Close')],
     ]
     # Create the Window
-    window = sg.Window('Conversation Helper', layout, finalize=True)
+    window = sg.Window('Vocal Assistant', layout, finalize=True)
     # Create the event loop
     chat_history_ids_list = []
     version_string = "0.1"
     welcome_string = f"\n\n" \
                      f" ***************************************\n" \
-                     f" Conversation Helper v" + version_string + " \n" \
+                     f" Vocal Assistant v" + version_string + " \n" \
                      f" ***************************************\n" \
-                     f"Do you wish you always knew what to say? Enter the Conversation Helper! This program uses GPT to suggest " \
-                     f"responses in conversations. By entering further replies, your answers will improve as the conversation flows. " \
-                     f"\n\nMade with <3 - https://github.com/mmusil25/conversation-helper" \
-                     f"\n\n\n" \
-                     f" Enter the message from your conversation partner"\
-                     " to receive suggested replies. Once you've chosen a reply or used your own, have sent the reply, and have received a response, Enter their " \
-                     "reply to receive a new batch of contextual responses based on previous messages. \n"\
-                     "\n(Note: This is a very large transformer and may appear to freeze. Please be patient.) \n" \
-                      "\n Try it! Enter their message in the white prompt box below.\n\n\n"
+                     f" Press start and to begin speaking with the AI. Speak when \"listening\" is printed." 
     window['-MLINE-'].update(welcome_string, append=True, autoscroll=True)
     window['-MLINE-'].update("", append=True, autoscroll=True)
 
     while True:
         event, values = window.read()
         try:
-            if event == 'Close':
+            if event == 'Close' or 'Stop':
                 # User closed the Window or hit the Cancel button
                 break
             elif event is None:
--- functional.py
+++ functional.py
@@ -13,6 +13,8 @@ from langchain.memory import ConversationBufferWindowMemory
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
 import pygame
+import pyaudio
+import wave
 #initialize the audio player
 pygame.init()
 #Speech recognition encoder
@@ -40,8 +42,34 @@ llm_chain = LLMChain(
 # download and load all TTS models
 preload_models(text_use_gpu=True, coarse_use_gpu=True, fine_use_gpu=True, codec_use_gpu=True)
 
-def listaudio_devices():
-    return
+def list_devices():
+    p = pyaudio.PyAudio()
+    device_count = p.get_device_count()
+    for i in range(0, device_count):
+        info = p.get_device_info_by_index(i)
+        print("Device {} = {}".format(info["index"], info["name"]))
+
+def list_input_devices(show=True):
+    input_device_array=[]
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range (0,numdevices):
+        if p.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
+                if show: print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name'))
+                input_device_array.append("Input Device id " + str(i) + " - " + p.get_device_info_by_host_api_device_index(0,i).get('name'))
+    return input_device_array
+
+def list_output_devices(show=True):
+    output_device_array = []
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range (0,numdevices):
+        if p.get_device_info_by_host_api_device_index(0,i).get('maxOutputChannels')>0:
+                if show: print("Output Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name'))
+                output_device_array.append("Output Device id " + str(i) + " - " + p.get_device_info_by_host_api_device_index(0,i).get('name'))
+    return output_device_array
 
 def listen(source, recognizer=r):
     recognizer.adjust_for_ambient_noise(source, duration=0.2)
@@ -51,7 +79,6 @@ def listen(source, recognizer=r):
     MyText = MyText.lower()
     return MyText
 
-
 def AI_response_to_speech(question):
     # use chat_model.predict to get the answer
     answer = chat_model.predict(question).strip()
@@ -59,25 +86,51 @@ def AI_response_to_speech(question):
     print(answer)
     return answer
 
+def get_first_integer(string):
+    return integer
+
+def pa_play_wav(filename, device = None):
+    CHUNK = 1023
+    with wave.open(filename, 'rb') as wf:
+        # Instantiate PyAudio and initialize PortAudio system resources (1)
+        p = pyaudio.PyAudio()
+
+        # Open stream (2)
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        # Play samples from the wave file (3)
+        while len(data := wf.readframes(CHUNK)):  # Requires Python 3.8+ for :=
+            stream.write(data)
+
+        # Close stream (4)
+        stream.close()
+
+        # Release PortAudio system resources (5)
+        p.terminate()
+
 def outputspeech(prompt, audio_name):
     audio_array = generate_audio(prompt, history_prompt="v2/en_speaker_3")
     # save audio to disk
     write_wav(audio_name, SAMPLE_RATE, audio_array)
-    my_sound = pygame.mixer.Sound(audio_name)
-    my_sound.play()
+    pa_play_wav(audio_name)
     return 
 
-with sr.Microphone() as source2:
-    i = 0
-    while True:
-        i = i + 1
-        try:
-            input = listen(source = source2, recognizer=r)
-        except:
-            continue
-        print("Customer input " + input)
-        ans = AI_response_to_speech(input)
-        outputspeech(ans, "/audio/tempAudio" + str(i) + ".wav")
+if __name__ == "__main__":
+    with sr.Microphone() as source2:
+        i = 0
+        while True:
+            i = i + 1
+            try:
+                input = listen(source = source2, recognizer=r)
+            except:
+                continue
+
+            print("Customer input " + input)
+            ans = AI_response_to_speech(input)
+            outputspeech(ans, "audio/tempAudio" + str(i) + ".wav")
 
 
 
